d9ef2f54008b3576c11b1a43f23e8a3b400da9cd
mcuongbush <nguyenmanhcuong.dev.it@gmail.com>
2022-09-24 15:54:43 +0700

    update

--- src/main/java/org/source/controllers/HomeController.java
+++ src/main/java/org/source/controllers/HomeController.java
@@ -2,10 +2,11 @@ package org.source.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping(value = {"/","/trang-chu"}, method = RequestMethod.GET)
     public String Index(){
         return "index";
     }
